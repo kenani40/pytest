beb5e82d8719c3cc1f50a3e10198011953d4b7d4
Gershom-bakili <gershombakili@gmail.com>
2026-09-21 06:55:35 +0200

    removed all comments

--- main.py
+++ main.py
@@ -1,24 +1,19 @@
-# Import the community
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
 
-# Set the style with Seaborn
 sns.set_style("darkgrid")
 
-# Let NumPy weave the data grid
 x = np.linspace(-5, 5, 50)
 y = np.linspace(-5, 5, 50)
 X, Y = np.meshgrid(x, y)
 Z = np.sin(np.sqrt(X**2 + Y**2))
 
-# Let Matplotlib craft the 3D visualization
 fig = plt.figure(figsize=(10, 8))
 ax = fig.add_subplot(111, projection='3d')
 surf = ax.plot_surface(X, Y, Z, cmap='viridis', alpha=0.8)
 
-# Add finishing touches
 fig.colorbar(surf, ax=ax, label='Z Value')
 ax.set_xlabel('X Axis')
 ax.set_ylabel('Y Axis')
